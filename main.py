4c95f9d86078814601cb0579e13efc065bd34048
DTheIcyDragon <paulnicolai.hennings@gmail.com>
2022-03-27 12:12:46 +0200

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 #class
 
 #definitions
-
+def get_prefix(client, message):
+    pass #have to look into my older projects
 #main_bot
-client = commands.Bot(command_prefix=)
+client = commands.Bot(command_prefix=get_prefix)
