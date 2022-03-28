93329ec8142a514cbd56a60626a0c11c6690bc95
DTheIcyDragon <paulnicolai.hennings@gmail.com>
2022-03-28 21:57:02 +0200

    Update

--- cogs/logging.py
+++ cogs/logging.py
@@ -4,10 +4,10 @@ from utils.design_helper import ConsoleColors
 import logging
 
 logging.basicConfig(filename="data/bot.log",
-                    filemode="w",
-                    datefmt="%d-%b-%y %H:%M:%S",
-                    format="%(levelname)s: %(asctime)s - %(message)s",
-                    level="INFO")
+            filemode="w",
+            datefmt="%d-%b-%y %H:%M:%S",
+            format="%(levelname)s: %(asctime)s - %(message)s",
+            level="INFO")
 
 class DiscordEvents(commands.Cog):
     def __init__(self, client):
@@ -16,17 +16,28 @@ class DiscordEvents(commands.Cog):
     @commands.Cog.listener()
     async def on_disconnect(self):
         logging.critical(f"The client has been disconnected!")
-        print(f"{ConsoleColors.RED} Disconnected")
+        print(f"{ConsoleColors.RED} Disconnected!")
 
     @commands.Cog.listener()
     async def on_resumed(self):
         logging.warning("The client has reconnected!")
-        print(f"{ConsoleColors.YELLOW} Reconnected")
-
+        print(f"{ConsoleColors.YELLOW} Reconnected!")
+        
     @commands.Cog.listener()
-    async def on_message_delete(self, message):
-        pass
-
+    async def on_member_join(self, member):
+        logging.info(f"{member.name}#{member.descriminator} joined the gang.")
+    
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        logging.info(f"{member.name}#{member.descriminator} left the gang.")
+        
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        logging.info(f"{self.client.user.name} joined {guild.name}.")
+        
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        logging.info(f"{self.client.user.name} left {guild.name}.")
 
 def setup(client):
     client.add_cog(DiscordEvents(client))
